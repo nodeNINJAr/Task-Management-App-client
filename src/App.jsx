25b203d0e9dd0e412d8b3266593e119376af4bbe
Mehedi hasan <nodeNINJAr@users.noreply.github.com>
2025-02-19 23:51:27 +0600

    project-setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-xl text-red-500 font-semibold text-center'>My task management App running</div>
+  )
+}
+
+export default App
